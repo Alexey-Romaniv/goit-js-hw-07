a41ce6f233cf72542dfbb1bccf3288bdff108711
Alexey-Romaniv <105770723+Alexey-Romaniv@users.noreply.github.com>
2022-08-29 09:18:04 +0300

    all done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,47 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
+
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryList = document.querySelector(".gallery");
+
+const markup = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<div class="gallery__item">
+  <a class="gallery__link" href=${original} >
+
+    <img
+      class="gallery__image"
+      src=${preview}
+      data-source=${original}
+      alt=${description}
+    />
+  </a>
+</div>`
+  )
+  .join("");
+
+galleryList.innerHTML = markup;
+
+galleryList.addEventListener("click", openModal);
+
+function openModal(e) {
+  e.preventDefault();
+  if (!e.target.classList.contains("gallery__image")) {
+    return;
+  }
+  const instance = basicLightbox.create(`
+  <img src=${e.target.dataset.source}>`);
+
+  instance.show();
+
+  galleryList.addEventListener("keydown", closeOnEsc);
+
+  function closeOnEsc(e) {
+    if (e.code === "Escape") {
+      instance.close();
+    }
+  }
+}
